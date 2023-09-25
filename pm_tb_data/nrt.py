f55e40af72beb0a2d8fc0d0772862fd206a03fc7
Trey Stafford <trey.stafford@colorado.edu>
2023-09-25 17:50:38 -0600

    Iterate over results and sort by revision date

--- pm_tb_data/nrt.py
+++ pm_tb_data/nrt.py
@@ -6,3 +6,9 @@ import earthaccess
 if __name__ == '__main__':
     results = earthaccess.search_data(short_name='AU_SI12_NRT_R04')
     print(f'Found {len(results)} granules.')
+    results = sorted(results, key=lambda x: x['meta']['revision-date'], reverse=True)
+    for granule in results:
+        # This looks like the fn
+        native_id = granule['meta']['native-id']
+        revision_date = granule['meta']['revision-date']
+        print(f'{native_id} {revision_date=}')
