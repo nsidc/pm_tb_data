e4ac61e0fbd08557f6c63d65459627bb3da9edd8
Trey Stafford <trey.stafford@colorado.edu>
2023-10-06 15:07:36 -0600

    Add an additional test case that includes a gap in data coverage

--- tests/unit/test_lance_amsr2.py
+++ tests/unit/test_lance_amsr2.py
@@ -63,6 +63,34 @@ def test__filter_out_last_day_unless_r04():
     assert third_date in filtered.keys()
 
 
+def test__filter_out_last_day_with_gap_r04():
+    latest_date = dt.date(2023, 10, 5)
+    second_date = latest_date - dt.timedelta(days=2)
+    third_date = latest_date - dt.timedelta(days=3)
+    mock_granules: nrt.GranuleInfoByDate = {
+        latest_date: {
+            "file_type": "P",
+            "filename": "AMSR_U2_L3_SeaIce12km_P04_20231005.he5",
+            "data_url": "www.example.com",
+        },
+        second_date: {
+            "file_type": "R",
+            "filename": "AMSR_U2_L3_SeaIce12km_R04_20231003.he5",
+            "data_url": "www.example.com",
+        },
+        third_date: {
+            "file_type": "R",
+            "filename": "AMSR_U2_L3_SeaIce12km_R04_20231002.he5",
+            "data_url": "www.example.com",
+        },
+    }
+
+    filtered = nrt._filter_out_last_day(granules_by_date=mock_granules)
+    assert latest_date not in filtered.keys()
+    assert second_date in filtered.keys()
+    assert third_date in filtered.keys()
+
+
 def test__get_granule_info_by_date():
     class MockDataGranule:
         def __init__(self):
