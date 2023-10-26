32de1ccaa23237b1e7fabd5b984e4816f80b6710
Trey Stafford <trey.stafford@colorado.edu>
2023-10-25 18:03:18 -0600

    Init work to define class/constants for hemisphere
    By default, this replacement type for `Hemisphere` should work seamlessely with
    existing code, _except_ where strings are passed around to anything typed with
    `Hemisphere`. Then we'll need to update that code to utilize the defined
    constants, `NORTH`, and `SOUTH`.

--- pm_tb_data/_types.py
+++ pm_tb_data/_types.py
@@ -1,3 +1,23 @@
 from typing import Literal
 
-Hemisphere = Literal["north", "south"]
+from pydantic import BaseModel
+
+
+class Hemisphere(BaseModel):
+    name: Literal["north", "south"]
+
+    @property
+    def initial(self) -> str:
+        return self.name[0].upper()
+
+    def __str__(self) -> str:
+        return self.name
+
+    # Allows e.g., `hemisphere[0]`, which would yeld `n` for the northern
+    # hemisphere.
+    def __getitem__(self, item):
+        return self.name[item]
+
+
+NORTH = Hemisphere(name="north")
+SOUTH = Hemisphere(name="south")
