77caf46b448829905990b7c34eea2f011ccb545f
Trey Stafford <trey.stafford@colorado.edu>
2023-10-26 08:08:58 -0600

    Hemisphere: use string literals but also define constants
    Class/pydantic model could be next step. For now want all projects to be able to
    use these constants. In the future, the literal string constants could be
    updated to be instances of a `Hemisphere` class

--- pm_tb_data/_types.py
+++ pm_tb_data/_types.py
@@ -1,23 +1,6 @@
-from typing import Literal
+from typing import Final, Literal
 
-from pydantic import BaseModel
+NORTH: Final = "north"
+SOUTH: Final = "south"
 
-
-class Hemisphere(BaseModel):
-    name: Literal["north", "south"]
-
-    @property
-    def initial(self) -> str:
-        return self.name[0].upper()
-
-    def __str__(self) -> str:
-        return self.name
-
-    # Allows e.g., `hemisphere[0]`, which would yeld `n` for the northern
-    # hemisphere.
-    def __getitem__(self, item):
-        return self.name[item]
-
-
-NORTH = Hemisphere(name="north")
-SOUTH = Hemisphere(name="south")
+Hemisphere = Literal[NORTH, SOUTH]
