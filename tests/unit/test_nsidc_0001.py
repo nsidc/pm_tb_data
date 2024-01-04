e8c83570b9d0c2a03f853a660d7570ea36ed1b7d
Trey Stafford <trey.stafford@colorado.edu>
2024-01-04 12:27:50 -0700

    Unit test for `_get_nsidc_0001_fp_on_disk`

--- tests/unit/test_nsidc_0001.py
+++ tests/unit/test_nsidc_0001.py
@@ -1,3 +1,6 @@
+import datetime as dt
+from pathlib import Path
+
 import numpy as np
 import xarray as xr
 from xarray.testing import assert_equal
@@ -27,3 +30,26 @@ def test__normalize_nsidc_0001_tbs():
     )
 
     assert_equal(actual, expected)
+
+
+def test_get_nsidc_0001_fp_on_disk(fs):
+    _data_dir = Path("/path/to/data/dir/2019.10.05")
+    _fake_files = [
+        _data_dir / "NSIDC0001_TB_PS_N25km_20191005_v6.0.nc",
+        _data_dir / "NSIDC0001_TB_PS_S25km_20191005_v6.0.nc",
+        _data_dir / "NSIDC0001_TB_PS_N12.5km_20191005_v6.0.nc",
+        _data_dir / "NSIDC0001_TB_PS_S12.5km_20191005_v6.0.nc",
+    ]
+    for _file in _fake_files:
+        fs.create_file(_file)
+
+    expected_file = _data_dir / "NSIDC0001_TB_PS_N25km_20191005_v6.0.nc"
+
+    actual = nsidc_0001.get_nsidc_0001_fp_on_disk(
+        data_dir=_data_dir,
+        date=dt.date(2019, 10, 5),
+        resolution="25",
+        hemisphere="north",
+    )
+
+    assert expected_file == actual
