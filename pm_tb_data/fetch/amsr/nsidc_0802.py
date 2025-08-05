44b7fcb8337a41767785b950cd2428147b45be19
Trey Stafford <trey.stafford@colorado.edu>
2025-08-05 16:54:12 -0600

    NSIDC-0802: Support reading 37h from prototype location

--- pm_tb_data/fetch/amsr/nsidc_0802.py
+++ pm_tb_data/fetch/amsr/nsidc_0802.py
@@ -9,6 +9,7 @@ import re
 from pathlib import Path
 
 import xarray as xr
+from loguru import logger
 
 from pm_tb_data._types import Hemisphere
 from pm_tb_data.fetch.nsidc_binary import read_binary_tb_file
@@ -19,6 +20,7 @@ def get_nsidc_0802_tbs_from_disk(
     date: dt.date,
     hemisphere: Hemisphere,
     data_dir: Path,
+    prototype_37h_data_dir: Path,
 ) -> xr.Dataset:
     """Return TB data from NSIDC-0802."""
     # This assumes `data_dir` points to the "nsidc0802_polar_stereo_tbs"
@@ -30,7 +32,17 @@ def get_nsidc_0802_tbs_from_disk(
     if not results:
         raise FileNotFoundError(f"No NSIDC-0007 TBs found for {date=} {hemisphere=}")
 
+    # Example fn: tb_as2_20240108_sfm_s37h.dat
+    prototype_37h_fn_glob = f"tb_as2_{date:%Y%m%d}_sfm_{hemisphere[0].lower()}37h.dat"
+    prototype_37h_results = list(prototype_37h_data_dir.rglob(prototype_37h_fn_glob))
+    prototype_37h_fp: None | Path = None
+    if len(prototype_37h_results) == 1:
+        prototype_37h_fp = prototype_37h_results[0]
+    else:
+        logger.warning(f"Failed to find 37h data for {date=}")
+
     tb_data_mapping = {}
+    # Published binary files
     tb_fn_re = re.compile(
         r"NSIDC-0802_TB_AMSR2_"
         + hemisphere[0].upper()
@@ -49,6 +61,21 @@ def get_nsidc_0802_tbs_from_disk(
             f"{match.group('polarization').lower()}{match.group('channel')}"
         ] = xr.DataArray(
             data,
+            dims=("fake_y", "fake_x"),
+            attrs={
+                "source_filename": tb_fp.name,
+            },
+        )
+
+    # prototype 37h
+    if prototype_37h_fp is not None:
+        prototype_37h_data = read_binary_tb_file(
+            filepath=prototype_37h_fp,
+            hemisphere=hemisphere,
+        )
+
+        tb_data_mapping["h37"] = xr.DataArray(
+            prototype_37h_data,
             dims=("fake_y", "fake_x"),
             attrs={
                 "source_filename": tb_fp.name,
--- tests/integration/test_nsidc_0802.py
+++ tests/integration/test_nsidc_0802.py
@@ -6,11 +6,20 @@ from pm_tb_data.fetch.amsr.nsidc_0802 import get_nsidc_0802_tbs_from_disk
 
 def test_get_nsidc_0802_tbs_from_disk():
     data_dir = Path("/disks/sidads_ftp/DATASETS/nsidc0802_polar_stereo_tbs")
+    # TODO: this path should be updated to something like
+    # `/share/apps/G02202_V6` once created and prototype data are staged there.
+    # eventually, the code to handle the prototype 37h data will be replaced
+    # with release ov 0802 v2, which will have all the necessary tbs in nc files.
+    prototype_37h_data_dir = Path("/home/vagrant/seaice_ecdr/nise_at_tbs")
     data = get_nsidc_0802_tbs_from_disk(
         date=dt.date(2025, 1, 1),
         hemisphere="north",
         data_dir=data_dir,
+        prototype_37h_data_dir=prototype_37h_data_dir,
     )
 
     assert "h19" in data.variables
+    assert "h37" in data.variables
+
     assert not data["h19"].isnull().all()
+    assert not data["h37"].isnull().all()
